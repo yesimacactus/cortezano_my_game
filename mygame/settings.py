4b2628d4d001af88537858de431c469a12eced72
yesimacactus <126900954+yesimacactus@users.noreply.github.com>
2023-03-09 12:01:34 -0800

    Add files via upload

---
+++ mygame/settings.py
@@ -0,0 +1,19 @@
+# File created by: Joshua Cortezano
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+# player attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+
+# colors
+BLACK = (0,0,0)
+BLUE = (50, 50, 255)
+
+
